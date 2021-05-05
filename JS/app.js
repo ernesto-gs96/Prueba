ce3013f024127e1a636091598a95c7e42de84e79
neto-gs <ernesto_gs96@hotmail.com>
2021-05-05 00:34:02 -0500

    Contador agregado

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,10 @@
 alert("Hola")
+
+const contador = document.querySelector("#Contador");
+contador.textContent = "0 segundos";
+
+let numero = 1;
+
+setInterval(() => {
+    contador.textContent = `${numero++} segundos`;
+},1000);
