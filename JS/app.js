9a8f13b1634cca94bea8f6b77f36f8bbb4b4c46e
neto-gs <ernesto_gs96@hotmail.com>
2021-05-05 03:10:33 -0500

    Funcionalidad a los botones agregada desde VS

--- JS/app.js
+++ JS/app.js
@@ -1,16 +1,28 @@
-alert("Hola")
-
 const contador = document.querySelector("#Contador");
 contador.textContent = "0 segundos";
 
 let segundos = 1;
 let minutos = 1;
 
-setInterval(() => {
+let intervalo = setInterval(tiempo,1000);
+function tiempo(){
     contador.textContent = `${segundos++} segundos`;
     if(segundos === 60*minutos){
         alert(`Ya paso ${minutos} minutos`);
         minutos++;
     }
-},1000);
+}
 
+const parar = () => {
+    clearInterval(intervalo);
+}
+
+// Si se preciona varias veces continuar ocurre un error
+const continuar = () => {
+    intervalo = setInterval(tiempo,1000);
+}
+
+const resetear = () => {
+    segundos = 0;
+    minutos = 1;
+}
